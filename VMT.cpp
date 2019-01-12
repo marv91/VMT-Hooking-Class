565e0cdb5de226a115f2b1125fb2191970b26e03
Marv <Marv@scc-wkit-clxa-26-49.scc.kit.edu>
2019-01-12 13:40:58 +0100

    Init
    VMT Hook provides a simple to use class for hooking virtual method tables.

---
+++ VMT.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+#include "VMT.h"
+
+
+VMTHook::VMTHook(void* ptr)
+{
+	this->SetVMT(ptr);
+}
+
+auto VMTHook::SetVMT(void* ptr) -> bool
+{
+		if (!ptr) return false;
+
+		m_pClass		=	ptr;
+		m_pOriginalVMT	=	*reinterpret_cast<uint32_t**>(ptr);
+
+		m_iVMTSize = 0;
+
+		while (m_pOriginalVMT[m_iVMTSize] && !IsBadReadPtr((void*)m_pOriginalVMT[m_iVMTSize], 4))
+			m_iVMTSize++;
+
+		m_pHookedVMT = new uint32_t[m_iVMTSize];
+
+		memcpy(
+			m_pHookedVMT, 
+			reinterpret_cast<const void*>(m_pOriginalVMT), 
+			m_iVMTSize++ * sizeof(void*)
+		);
+		
+		*reinterpret_cast<uint32_t**>(m_pClass) = m_pHookedVMT;
+
+		return true;
+}
+
+auto VMTHook::RestoreOriginalVMT() -> void
+{
+	*reinterpret_cast<uint32_t**>(m_pClass) = m_pOriginalVMT;
+	delete[] m_pHookedVMT;
+}
+
+auto VMTHook::HookFunction(uint32_t index, void *hkFunction) -> void*
+{
+	if (index > m_iVMTSize - 1) return nullptr;
+	m_pHookedVMT[index] = reinterpret_cast<uint32_t>(hkFunction);
+	return reinterpret_cast<void*>(m_pOriginalVMT[index]);
+}
+
+auto VMTHook::UnhookVMT() -> void
+{
+	*reinterpret_cast<uint32_t**>(m_pClass) = m_pOriginalVMT;
+}
+
+auto VMTHook::RehookVMT() -> void
+{
+	*reinterpret_cast<uint32_t**>(m_pClass) = m_pHookedVMT;
+}
+
+
---
+++ VMT.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <Windows.h>
+#include <stdint.h>
+
+class VMTHook
+{
+public:
+	
+	VMTHook()	= default;
+	~VMTHook() = default;
+
+	VMTHook(void* ptr);
+
+	virtual auto UnhookVMT()			-> void;
+	virtual auto RehookVMT()			-> void;
+	virtual auto RestoreOriginalVMT()	-> void;
+
+	template <class T>
+	static VMTHook* Create(T* ptr) {
+		return new VMTHook(reinterpret_cast<void*>(ptr));
+	}
+
+	template <class T>
+	auto HookFunction(uint32_t index, void* hkFunction) -> T
+	{
+		return reinterpret_cast<T>(HookFunction(index, hkFunction));
+	}
+	
+
+private:
+
+	virtual auto HookFunction(uint32_t index, void* hkFunction) -> void*;
+	virtual auto SetVMT(void*)									-> bool;
+
+	void*		m_pClass;
+	size_t		m_iVMTSize;
+	uint32_t*	m_pOriginalVMT;
+	uint32_t*	m_pHookedVMT;
+	
+
+	
+};
+
+//extern CVMT* VMTClient;
+
+
+	
+
